359f106e5393d982b3285b36c405055b933849e1
Fergal McNamara <g00180082@atu.ie>
2025-09-27 15:17:10 +0100

    Adding method to RequestController

---
+++ src/main/java/ie/atu/postlab1/RequestController.java
@@ -0,0 +1,17 @@
+package ie.atu.postlab1;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping
+
+public class RequestController {
+@GetMapping("/hello")
+    public String hello() {
+    return "Hello";
+    }
+}
