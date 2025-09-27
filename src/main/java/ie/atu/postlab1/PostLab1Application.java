5fe9599c072fee7d4c1fba970cd5f2ad9e50a7f6
Fergal McNamara <g00180082@atu.ie>
2025-09-27 14:13:43 +0100

    Initial commit

---
+++ src/main/java/ie/atu/postlab1/PostLab1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.postlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/postlab1/PostLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.postlab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
